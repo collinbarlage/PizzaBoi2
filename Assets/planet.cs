e213c2e4a2b2db37c70d0ba1458eb2162d7d3261
Collin Barlage <cjb356@drexel.edu>
2018-09-04 10:32:12 -0400

    Add planet

---
+++ Assets/planet.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class planet : MonoBehaviour {
+    public Rigidbody rb;
+    public Text eText;
+
+    private string name;
+
+	void Start () {
+        rb.AddTorque(0, 3f, 0);
+
+        // set name
+        name = "Diga IV";
+	}
+
+	private void OnTriggerEnter(Collider other) {
+        if (other.name == "spaceship3") {
+        	Debug.Log("~~~~~~~~~~~~~~~~~BIG MAMA");
+            eText.text = "Press 'e' to enter planet " + name;
+        }
+    }
+    private void OnTriggerExit(Collider other) {
+        if (other.name == "spaceship3") {
+            eText.text = "";
+        }
+    }
+
+}
